55312128acd5bfa624071d1cd7fab48e18fb0422
sarfaraz-zaptech <sarfaraz@zaptechsolutions.com>
2020-08-19 20:39:10 +0530

    test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ rm -rf /var/www/html/*
 #echo "<?php phpinfo(); ?>" > /var/www/html/index.php
 sed -i "s/display_errors = Off/display_errors = On/" /etc/php/7.0/apache2/php.ini
 sed -i "s/max_execution_time = 30/max_execution_time = 0/" /etc/php/7.0/apache2/php.ini
-#sed -i "s/error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT/error_reporting = E_WARNING & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT/" /etc/php/7.0/apache2/php.ini
+sed -i 's/^error_reporting = .*/error_reporting = E_WARNING \& ~E_NOTICE \& ~E_DEPRECATED \& ~E_STRICT/' /etc/php/7.0/apache2/php.ini
 sed -i "s/log_errors = On/log_errors = Off/" /etc/php/7.0/apache2/php.ini
 sed -i "s/short_open_tag = Off/short_open_tag = On/" /etc/php/7.0/apache2/php.ini
 
